9d9cbde4e7c529c0cda39cee4528e15903117a50
gmoroz <12345hasek@gmail.com>
2023-01-27 19:30:55 +0300

    create menu model

--- nested_menu/models.py
+++ nested_menu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название меню")
+    url = models.CharField(max_length=200, verbose_name="url меню")
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, default=None, related_name="child"
+    )
+
+    class Meta:
+        verbose_name = "Меню"
+        verbose_name_plural = "Меню"
